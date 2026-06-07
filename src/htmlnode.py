eba5c524a7e314c5dea70fc9c5a0c30ca2f1b703
PD3Dev <=danny.preusker@pm.me>
2026-06-07 21:44:02 +0200

    Up to CH2 L5

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,16 +6,44 @@ class HTMLNode:
         self.children = children
         self.props = props
 
-        def to_html(self):
+    def to_html(self):
             raise NotImplementedError
 
-        def props_to_html(self):
+    def props_to_html(self):
+        if self.props is not None:
             keys = sorted(list(self.props))
             prop_str = ""
 
             for key in keys:
                 prop_str += f'{key}="{self.props[key]}" '
             return prop_str
+        else:
+            return None
 
-        def __repr__(self):
+    def __repr__(self):
             print(f'HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})')
+
+    def __eq__(self, other):
+        if (self.tag == other.tag
+            and
+            self.value == other.value
+            and
+            self.children == other.children
+            and
+            self.props == other.props):
+            return True
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+         super().__init__(tag, value, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        if self.tag == None:
+            return str(self.value)
+        
+        return f'<{self.tag}>{self.value}</{self.tag}>'
+    
+    def __repr__(self):
+         print(f'HTMLNode({self.tag}, {self.value}, {self.props})')
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,9 +3,31 @@ import unittest
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
-        node = HTMLNode("p", "This is a value text")
-        node2 = HTMLNode("p", "This is a value text")
-        self.assertEqual(node, node2)
+        htmlnode = HTMLNode("p", "This is a value text")
+        htmlnode2 = HTMLNode("p", "This is a value text")
+        self.assertEqual(htmlnode, htmlnode2)
+        htmlnode5 = HTMLNode("p", "This is a value text")
+        htmlnode6 = HTMLNode("p", "This is a value text", None, None)
+        self.assertEqual(htmlnode5, htmlnode6)
+
+    def test_uneq(self):
+        child1 = HTMLNode("a", "text")
+        child2 = HTMLNode("a", "diff text")
+        htmlnode3 = HTMLNode("i", "some text", [child1], {"href": "google.de"})
+        htmlnode4 = HTMLNode("i", "some text", [child2], {"href": "google.de"})
+        self.assertNotEqual(htmlnode3, htmlnode4)
+
+    def test_proptohtml(self):
+        htmlnode7 = HTMLNode("i", "some text", None, {"href": "google.de"})
+        test_str = htmlnode7.props_to_html()
+        ctrl_str = 'href="google.de" '
+        self.assertEqual(test_str, ctrl_str)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        node2 = LeafNode("a", "Hello, world!", {"href": "google.de"})
+        self.assertEqual(node2.to_html(), "<a>Hello, world!</a>")
 
 if __name__ == "__main__":
     unittest.main()
