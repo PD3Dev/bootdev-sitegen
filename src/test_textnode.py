7c9d3edf0bb18289cb6b90043f4f8806b14acfd9
PD3Dev <=danny.preusker@pm.me>
2026-06-11 19:16:02 +0200

    Text to HTML tests done. Netxt lesson

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -15,6 +15,25 @@ class TestTextNode(unittest.TestCase):
         node5 = TextNode("This is a link node", TextType.LINK, "https://gidf.de")
         node6 = TextNode("This is a text node", TextType.ITALIC)
         self.assertNotEqual(node5, node6)
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("", TextType.IMAGE, "https://boot.dev/logo.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.props, {'src': 'https://boot.dev/logo.png', 'alt': ''})
+
+    def test_link(self):
+        node = TextNode("This is a link node", TextType.LINK, "https://google.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link node")
+        self.assertEqual(html_node.props, {'href': 'https://google.com'})
 
 if __name__ == "__main__":
     unittest.main()
