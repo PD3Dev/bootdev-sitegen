e64e536cf8d2e7aac7b3af71f7d211b50db4806a
PD3Dev <=danny.preusker@pm.me>
2026-07-02 22:08:00 +0200

    Delete public folder done and implemented.

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,4 +1,8 @@
 import os
+import shutil
+def del_public():
+    shutil.rmtree('public/')
+
 def copy_static():
     print('Hello')
     current_directory = os.listdir('.')
--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from textnode import *
 from textnode import TextNode, TextType
 from convert import extract_markdown_links, split_nodes_link
-from copystatic import copy_static
+from copystatic import copy_static, del_public
 def main():
 
+    del_public()
     copy_static()
 
 if __name__ == "__main__":
